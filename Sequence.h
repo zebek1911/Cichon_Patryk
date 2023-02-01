be19b8bac62240aa993e01066a868b288e189057
zebek1911 <48102367+zebek1911@users.noreply.github.com>
2023-02-01 14:01:07 +0100

    Add files via upload

--- Sequence.h
+++ Sequence.h
@@ -1,32 +1,97 @@
 #ifndef SEQUENCE_H
 #define SEQUENCE_H
 
+/**
+ * @class Sequence
+ * @brief A class representing a mathematical sequence of integers.
+ */
 class Sequence
 {
 public:
-    //konstruktor domyślny
+    /**
+     * @brief Default constructor
+     * 
+     * Constructs a new object of the class with default values.
+     */
     Sequence();
 
-    //konstruktor kopiujący
+    /**
+     * @brief Copy constructor
+     * 
+     * Constructs a new object as a copy of an existing 'Sequence' object.
+     * 
+     * @param other A constant reference to the existing 'Sequence' object.
+     */
     Sequence(const Sequence& other);
 
-    //destruktor
+    /**
+     * @brief Destructor
+     * 
+     * Cleans up memory when the object goes out of scope.
+     */
     ~Sequence();
 
-    //funkcje dostępu do składowych
+    /**
+     * @brief Get the length of the sequence
+     * 
+     * @return The length of the sequence as an integer.
+     */
     int getLength();
+
+    /**
+     * @brief Set the length of the sequence
+     * 
+     * @param newLength The new length of the sequence as an integer.
+     */
     void setLength(int newLength);
 
+    /**
+     * @brief Get the element at a given index
+     * 
+     * @param index The index of the element to be retrieved.
+     * 
+     * @return The value of the element at the given index.
+     */
     int getElement(int index);
+
+    /**
+     * @brief Set the element at a given index
+     * 
+     * @param index The index of the element to be set.
+     * @param newValue The new value of the element.
+     */
     void setElement(int index, int newValue);
 
-    //funkcje obliczeniowe
+    /**
+     * @brief Calculate the sum of all elements in the sequence
+     * 
+     * @return The sum of all elements in the sequence as an integer.
+     */
     int calculateSum();
+
+    /**
+     * @brief Calculate the product of all elements in the sequence
+     * 
+     * @return The product of all elements in the sequence as an integer.
+     */
     int calculateProduct();
+
+    /**
+     * @brief Calculate the average of all elements in the sequence
+     * 
+     * @return The average of all elements in the sequence as a double.
+     */
     double calculateAverage();
 
 private:
+    /**
+     * @brief The length of the sequence
+     */
     int length;
+
+    /**
+     * @brief A pointer to an array of integers representing the elements of the sequence
+     */
     int* data;
 };
 
--- Statistics.h
+++ Statistics.h
@@ -1,27 +1,83 @@
+/**
+ * @file Statistics.h
+ * @brief Plik nagłówkowy klasy Statistics
+ * 
+ * Plik zawiera definicję klasy Statistics oraz jej metod publicznych i prywatnych.
+ */
+
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
+/**
+ * @class Statistics
+ * @brief Klasa do obliczania statystyk
+ * 
+ * Klasa pozwala na obliczanie średniej oraz odchylenia standardowego danych.
+ */
 class Statistics
 {
     public:
-        //konstruktor domyślny
+        /**
+         * @brief Konstruktor domyślny
+         * 
+         * Tworzy nową instancję klasy Statistics z wartością 0 przypisaną do składowej data.
+         */
         Statistics();
 
-        //konstruktor kopiujący
+        /**
+         * @brief Konstruktor kopiujący
+         * 
+         * Tworzy nową instancję klasy Statistics skopiowaną z innej instancji.
+         * 
+         * @param other - referencja do instancji klasy Statistics, z której nastąpi skopiowanie.
+         */
         Statistics(const Statistics& other);
 
-        //destruktor
+        /**
+         * @brief Destruktor
+         * 
+         * Usuwa instancję klasy Statistics.
+         */
         ~Statistics();
 
-        //funkcje dostępu do składowych
+        /**
+         * @brief Funkcja dostępu do składowej data
+         * 
+         * @return Wartość składowej data.
+         */
         int getData();
+
+        /**
+         * @brief Funkcja ustawiania składowej data
+         * 
+         * @param newData - nowa wartość składowej data.
+         */
         void setData(int newData);
 
-        //funkcje obliczeniowe
+        /**
+         * @brief Funkcja obliczania średniej
+         * 
+         * Oblicza średnią wartość danych.
+         * 
+         * @return Średnia wartość danych.
+         */
         int calculateMean();
+
+        /**
+         * @brief Funkcja obliczania odchylenia standardowego
+         * 
+         * Oblicza odchylenie standardowe danych.
+         * 
+         * @return Odchylenie standardowe danych.
+         */
         int calculateStandardDeviation();
 
     private:
+        /**
+         * @brief Składowa data
+         * 
+         * Przechowuje wartość danych.
+         */
         int data;
 };
 
